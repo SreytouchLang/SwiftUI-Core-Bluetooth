ab9a3afb37ef4ffd86de7729f27292fb2d86ba73
SreytouchLang <atouch.it@gmail.com>
2024-01-02 11:33:13 -0800

    Initial Commit

---
+++ SwiftUI-Core-Bluetooth/SwiftUI_Core_BluetoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Core_BluetoothApp.swift
+//  SwiftUI-Core-Bluetooth
+//
+//  Created by Sreytouch(Jessica) on 1/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Core_BluetoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
